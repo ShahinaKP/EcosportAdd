8b7cee904ad5a594d2e0006ef34ead977ed904e4
ShahinaKP <shahina@qburst.com>
2015-09-23 17:44:40 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    setTimeout(function(){
+    	$('#ray').css({opacity:1});}, 3000);
+    setTimeout(function(){
+    	$('#logo').addClass('logoZoomEffect');}, 4000);
+    setTimeout(function(){
+    	$('#banner1').css("display", "block");}, 5000);
+
+});
