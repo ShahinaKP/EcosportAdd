047a1bdfa6545d1bda19e667df074c39a3aac51d
ShahinaKP <shahina@qburst.com>
2015-09-28 17:56:13 +0530

    Adde3d more image and texts

--- script.js
+++ script.js
@@ -4,6 +4,28 @@ $( document ).ready(function() {
     setTimeout(function(){
     	$('#logo').addClass('logoZoomEffect');}, 4000);
     setTimeout(function(){
-    	$('#banner1').css("display", "block");}, 5000);
+    	$('#banner1').css({opacity:1});
+    	$('#banner1Text').css({opacity:1});
+    }, 5000);
+    setTimeout(function(){
+    	$('#banner1').css({opacity:0});
+    	$('#banner1Text').css({opacity:0});    	
+    }, 6000);
+    setTimeout(function(){    	
+    	$('#banner2').css({opacity:1});
+    }, 6500);
+    setTimeout(function(){    	
+    	$('#banner2Text').css({opacity:1});
+    }, 7000);
+     setTimeout(function(){
+    	$('#banner2').css({opacity:0});
+    	$('#banner2Text').css({opacity:0});    	
+    }, 8000);
+     setTimeout(function(){    	
+    	$('#banner3').css({opacity:1});
+    }, 8500);
+    setTimeout(function(){    	
+    	$('#banner4Text').css({opacity:1});
+    }, 9000);
 
 });
